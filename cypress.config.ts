01e0426f125e9bd5945bf6a0003dfe3c099dfc7a
Robert Kelly <robertkelly1203@gmail.com>
2022-12-21 11:31:21 +0100

    Cypress CI added

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    video: false,
   },
 });
